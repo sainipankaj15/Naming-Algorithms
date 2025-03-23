5187f2c2acbd1f157ad56fcf9d933f04ce320d0f
saini <pksmks1999@gmail.com>
2025-03-23 20:40:55 +0530

    Refactored : Dutch National Flag

--- Array/Dutch_national_flag_algorithm.cpp
+++ Array/Dutch_national_flag_algorithm.cpp
@@ -1,38 +1,41 @@
 //https://leetcode.com/problems/sort-colors/
 // dutch national flag algorithm
-class Solution
-{
-public:
-    void sortColors(vector<int> &nums)
-    {
 
-        int low = 0;
-        int mid = 0;
-        int high = nums.size() - 1;
-        // what we will do
-        // if nums[mid] =  1 then mid++;
-        // if nums[mid] = 0 then low and mid dono ++; and also swapped the value
-        // if nums[mid] =  2 then mid++ and high--; and also swappped the value
-
-        while (mid <= high)
-        {
-            if (nums[mid] == 0)
-            {
-                // swap mid and low
-                swap(nums[mid], nums[low]);
-                mid++;
-                low++;
+class Solution {
+    public:
+        void sortColors(vector<int>& nums) {
+            
+            // This called Dutch National Flag Algorithm
+            int len = nums.size();
+    
+            // low will handle zero number
+            int low = 0;
+            // mid will handle 1 number
+            int mid = 0;
+            // high will handle 2 number
+            int high = len-1;
+    
+            while(mid <= high){
+                // if 0 comes 
+                if (nums[mid]==0){
+                    // low and mid dono ++; and also swapped the value
+                    swap(nums[low],nums[mid]);
+                    low++;
+                    mid++;
+                }
+    
+                // if 1 comes
+                else if (nums[mid]==1){
+                    mid++;
+                }
+    
+                // if 2 comes
+                else{
+                    // high--; and also swappped the value
+                    swap(nums[mid],nums[high]);
+                    high--;
+                }
             }
-            else if (nums[mid] == 1)
-            {
-                mid++;
-            }
-            else
-            {
-                // swap mid and high
-                swap(nums[mid], nums[high]);
-                high--;
-            }
+    
         }
-    }
-};
+    };
--- Array/Floyd_algorithm+_torute_and_hare_method.cpp
+++ Array/Floyd_algorithm+_torute_and_hare_method.cpp
@@ -1,17 +1,18 @@
 // https://leetcode.com/problems/find-the-duplicate-number/
-// Floyd's Algorithm  = Tortois and Hare method
-// cycle detection k liye
-// I saw three video anuj bhaiya , neetcode video and striver then samaje aaya
+// Floyd's Cycle Algorithm  = Tortois and Hare method
+// https://www.geeksforgeeks.org/floyds-cycle-finding-algorithm/
+// Here we will assume every element in array as a Node of Linked list
 
 
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-       // 
-       
+        
+        // we will use tortoise and hare method       
         int slow =  nums[0];
         int fast = nums[0];
         
+        // slow will move by 1 and fast will move by 2
         do{
             slow = nums[slow];
             fast = nums[nums[fast]];
@@ -22,9 +23,9 @@ public:
         // till now both has meet 
         // so now we will set a new pointer to first position which is also type of slow 
         // and from here we will move this slow pointer 
-       // where they will meet it is our answer
+        // where they will meet it is our answer
             
-            fast = nums[0];
+        fast = nums[0];
         
         while(fast!=slow){
             fast = nums[fast];
@@ -32,7 +33,6 @@ public:
         }
         
         // now slow and fast are equal return anyone
-        
         return slow;
     }
 };
